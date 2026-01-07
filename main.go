d68157096aa56c10294862ebd337e209f110aa7d
smpl <sreddy@hey.com>
2026-01-07 14:41:24 -0500

    [git save]

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+// TODO 1: connect to twitch stream
+
+// TODO 2: Print the stream
+
+// TODO 3: Initiate translator
+
+// TODO 4.1: each message into separate thread
+// TODO 4.2: multithread convert messages
+// TODO 4.3: collect messages, and print (order doesnt matter for now)
+
 func main() {
 	fmt.Println("TWITCH-LINGO")
 }
